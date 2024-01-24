7ece32da254450f6c0a5d1205121c2d09d83b789
notyetspecified <jtladeiras@gmail.com>
2024-01-24 10:28:35 +0000

    fix(i2c): moved to launcher

--- pisugar/install.sh
+++ pisugar/install.sh
@@ -19,25 +19,19 @@ echo "Selected ${psmodel}"
 # configure
 echo "Configuring PiSugar..."
 debconf-set-selections << EOF
-pisugar-server pisugar-server/model select ${psmodel}
-pisugar-server pisugar-server/auth-username string ${psusername}
-pisugar-server pisugar-server/auth-password password ${pspassword}
+pisugar-server pisugar-server/model select "${psmodel}"
+pisugar-server pisugar-server/auth-username string "${psusername}"
+pisugar-server pisugar-server/auth-password password "${pspassword}"
 EOF
 DEBIAN_FRONTEND=noninteractive dpkg -i $(pwd)/dist/pisugar-server*.deb
-rm $(pwd)/dist/pisugar-server*.deb
+#rm $(pwd)/dist/pisugar-server*.deb
 
 debconf-set-selections << EOF
-pisugar-poweroff pisugar-poweroff/model select ${psmodel}
+pisugar-poweroff pisugar-poweroff/model select "${psmodel}"
 EOF
 DEBIAN_FRONTEND=noninteractive dpkg -i $(pwd)/dist/pisugar-poweroff*.deb
-rm $(pwd)/dist/pisugar-poweroff*.deb
+#rm $(pwd)/dist/pisugar-poweroff*.deb
 
-mkdir -p /usr/local/share/pwnagotchi/custom-plugins
 ln -s $(pwd)/dist/pisugar2py /usr/local/lib/python3.7/dist-packages/pisugar2py
 ln -s $(pwd)/dist/pwnagotchi-pisugar2-plugin/pisugar2.py /usr/local/share/pwnagotchi/custom-plugins/pisugar2.py
-cat > /usr/local/share/pwnagotchi/custom-plugins/pisugar2.yml << EOF
-pisugar2:
-  visible: true
-  enabled: true
-EOF
 
