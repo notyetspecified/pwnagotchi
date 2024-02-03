6b2f6fbf6d27bcc25de2f21e7de5a1e09d841280
notyetspecified <jtladeiras@gmail.com>
2024-02-03 12:52:23 +0000

    fix(pisugar): patch corrections/improvements

--- pisugar/install.sh
+++ pisugar/install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# variables - TODO: make these configurable
+echo "Patching Pwnagotchi..."
+
+### PiSugar
 psmodel="PiSugar 2 (2-LEDs)"
 psusername="admin"
 pspassword="admin"
-
 # uninstall if already installed
 if [ -f /usr/local/lib/python3.7/dist-packages/pisugar2py ]; then
   echo "Uninstalling PiSugar..."
@@ -12,26 +13,35 @@ if [ -f /usr/local/lib/python3.7/dist-packages/pisugar2py ]; then
   rm -rf /usr/local/lib/python3.7/dist-packages/pisugar2py
   rm -rf /usr/local/share/pwnagotchi/custom-plugins/pisugar2.py
 fi
-
 # start
-echo "Selected ${psmodel}"
-
+echo "Selected model: ${psmodel}"
 # configure
-echo "Configuring PiSugar..."
+echo "Installing PiSugar server..."
 debconf-set-selections << EOF
-pisugar-server pisugar-server/model select "${psmodel}"
-pisugar-server pisugar-server/auth-username string "${psusername}"
-pisugar-server pisugar-server/auth-password password "${pspassword}"
+pisugar-server pisugar-server/model select ${psmodel}
+pisugar-server pisugar-server/auth-username string ${psusername}
+pisugar-server pisugar-server/auth-password password ${pspassword}
 EOF
 DEBIAN_FRONTEND=noninteractive dpkg -i $(pwd)/dist/pisugar-server*.deb
-#rm $(pwd)/dist/pisugar-server*.deb
-
+echo "Installing PiSugar poweroff..."
 debconf-set-selections << EOF
 pisugar-poweroff pisugar-poweroff/model select "${psmodel}"
 EOF
 DEBIAN_FRONTEND=noninteractive dpkg -i $(pwd)/dist/pisugar-poweroff*.deb
-#rm $(pwd)/dist/pisugar-poweroff*.deb
+echo "Installing PiSugar Python library..."
+rm -rf /usr/local/lib/python3.7/dist-packages/pisugar2py
+sudo cp -r ./dist/pisugar2py /usr/local/lib/python3.7/dist-packages
+cp ./dist/pwnagotchi-pisugar2-plugin/pisugar2.py /usr/local/share/pwnagotchi/custom-plugins/pisugar2.py
 
-ln -s $(pwd)/dist/pisugar2py /usr/local/lib/python3.7/dist-packages/pisugar2py
-ln -s $(pwd)/dist/pwnagotchi-pisugar2-plugin/pisugar2.py /usr/local/share/pwnagotchi/custom-plugins/pisugar2.py
+# install other dependencies
+echo "Installing Pwnagotchi custom plugins dependencies..."
+DEBIAN_FRONTEND=noninteractive dpkg -i $(pwd)/dist/hwloc*.deb
+DEBIAN_FRONTEND=noninteractive dpkg -i $(pwd)/dist/aircrack-ng*.deb
+pip3 install ./dist/protobuf*.whl
 
+# create /usr/share/wordlists if it doesn't exist
+echo "Copying wordlists..."
+mkdir -p /usr/share/wordlists
+cp -r ./dist/wordlists/* /usr/share/wordlists
+
+echo "Done patching!"
